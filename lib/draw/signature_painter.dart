0e0ec84b612e47de0124482e2c54866f2faacbfd
Eugene-Lam <eugenelam98@gmail.com>
2022-01-29 17:41:57 +0800

    .

--- lib/draw/signature_painter.dart
+++ lib/draw/signature_painter.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:droom_flutter/draw/draw_entity.dart';
-import 'package:droom_flutter/draw/draw_provider.dart';
+import '/draw/draw_entity.dart';
+//import '/draw/draw_provider.dart';
 
-class SignaturePainter extends CustomPainter {
-  late List<DrawEntity> pointsList;
+class SignaturePainter extends CustomClipper {
+  List<DrawEntity> pointsList;
   late Paint pt;
-
-  SignaturePainter(this.pointsList) {
+  SignaturePainter(this.pointsList){
     pt = Paint()
-      ..color = pintColor["default"] as Color
+      //..color = pintColor['default'] as Color
       ..strokeCap = StrokeCap.round
       ..isAntiAlias = true
       ..strokeWidth = 3.0
       ..style = PaintingStyle.stroke
       ..strokeJoin = StrokeJoin.bevel;
   }
-
-  void paint(Canvas canvas, Size size) {
+  void paint (Canvas canvas,Size size){
     for (int i = 0; i < pointsList.length - 1; i++) {
+      //画线
       if (pointsList[i] != null && pointsList[i + 1] != null) {
+        // ignore: avoid_single_cascade_in_expression_statements
         pt
-          ..color = pintColor[pointsList[i].color] as Color
+          //..color = pintColor[pointsList[i].color]
           ..strokeWidth = pointsList[i].strokeWidth;
 
         canvas.drawLine(pointsList[i].offset, pointsList[i + 1].offset, pt);
@@ -28,5 +28,13 @@ class SignaturePainter extends CustomPainter {
     }
   }
 
+  bool shouldRepaint(SignaturePainter other) => other.pointsList != pointsList;
+}
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
   bool shouldRepaint(SignaturePainter other) => other.pointsList != pointsList;
 }
