cdf215ceeb425d381364d61632e34b31933e7bb3
kelvin1268 <kelvinw1019@gmail.com>
2022-01-29 23:17:37 +0800

    loading

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'loading.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final bool _SerIsReady = true;
+  late  bool loading = false;
+  final _controllerID = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return loading ? Loading() : MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: BoxDecoration(
@@ -20,7 +30,7 @@ class Home extends StatelessWidget {
               child: Column(
                 children: [
                   Expanded(
-                    flex: 1,
+                    flex: 6,
                     child: Container(
                       decoration: BoxDecoration(
                         color: Colors.white54,
@@ -51,17 +61,23 @@ class Home extends StatelessWidget {
                               ),
                             ),
                           ),
+                          Spacer(flex: 2),
                           Container(
                             alignment: Alignment.topLeft,
-                            child: Flexible(
-                              flex: 2,
-                                child: Text(
-                                    "For Teacher:",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold
-                                  ),
-                                )
+                            child: Row(
+                              children: [
+                                Flexible(
+                                  flex: 2,
+                                    child: Text(
+                                        "For Teacher:",
+                                      style: TextStyle(
+                                          fontFamily: 'Mochiypop',
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold
+                                      ),
+                                    )
+                                ),
+                              ],
                             ),
                           ),
                           Expanded(
@@ -71,10 +87,11 @@ class Home extends StatelessWidget {
                               child: Text(
                                 "1. CREATE your room\n2. DRAW questions/explanations on paper \n3. GET feedbacks from student's writing \n4. ENJOY INTERACTION on paper!",
                                 style: TextStyle(
+                                    fontFamily: 'Mochiypop',
                                     color: Colors.grey[800],
-                                  fontWeight: FontWeight.bold,
-                                  fontStyle: FontStyle.italic,
-                                  fontSize: 15
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 14
                                 ),
                               ),
                             ),
@@ -86,8 +103,9 @@ class Home extends StatelessWidget {
                                 child: Text(
                                   'For Students:',
                                   style: TextStyle(
+                                    fontFamily: 'Mochiypop',
                                     fontWeight: FontWeight.bold,
-                                    fontSize: 18,
+                                    fontSize: 15,
                                   ),
                                 ),
                               )
@@ -99,10 +117,11 @@ class Home extends StatelessWidget {
                               child: Text(
                                   "1. LOOK at Teacher's Paper\n2.GIVE feedbacks/answers on paper\n3.ENJOY INTERACTION on paper!",
                                 style: TextStyle(
+                                    fontFamily: 'Mochiypop',
                                     color: Colors.grey[800],
                                   fontWeight: FontWeight.bold,
                                   fontStyle: FontStyle.italic,
-                                  fontSize: 15
+                                  fontSize: 14
                                 ),
                               ),
                             ),
@@ -112,51 +131,118 @@ class Home extends StatelessWidget {
                     )
                   ),
                   Expanded(
-                    flex: 1,
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: MaterialButton(
-                        color: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
+                    flex: 5,
+                    child: Column(
+                      children: [
+                        Flexible(
+                          flex: 5,
+                          child: Container(
+                            alignment: Alignment.center,
+                            // create (for teacher)
+                            child: MaterialButton(
+                              color: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              elevation: 10,
+                              onPressed: () async {
+                                //change to loading screen until server data is ready,
+                                if (_SerIsReady == false) {
+                                  setState(() {
+                                    loading = true;
+                                  });
+                                }
+                                if ( _SerIsReady == true) {
+                                  setState(() {
+                                    loading = false;
+                                  });
+                                  Navigator.pushNamed(
+                                      context,
+                                      '/drawroom',
+                                      arguments: {"roomid": "18"}
+                                  );
+                                }
+                              },
+                              child: Text(
+                                  "create",
+                                style: TextStyle(
+                                  fontFamily: 'Mochiypop',
+                                ),
+                              ),
+                            )
+                          ),
                         ),
-                        elevation: 10,
-                        onPressed: (){
-                          if ( _SerIsReady == true) {
-                            Navigator.pushNamed(
-                              context,
-                              '/drawroom',
-                              arguments: {"roomid": "18"}
-                            );
-                          }
-                        },
-                        child: Text("create"),
-                      )
+                        Flexible(
+                            flex: 5,
+                            child: Row(
+                              children: [
+                                Flexible(
+                                  flex:4,
+                                  child: MaterialButton(
+                                    color: Colors.red,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                    elevation: 10,
+                                    onPressed: () async {
+                                      //change to loading screen until server data is ready,
+                                      if (_SerIsReady == false) {
+                                        setState(() {
+                                          loading = true;
+                                        });
+                                      }
+                                      if ( _SerIsReady == true) {
+                                        setState(() {
+                                          loading = false;
+                                        });
+                                        Navigator.pushNamed(
+                                            context,
+                                            '/drawroom',
+                                            arguments: {"roomid": "18"}
+                                        );
+                                      }
+                                    },
+                                    child: Text(
+                                      "Join",
+                                      style: TextStyle(
+                                        fontFamily: 'Mochiypop',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Spacer(flex: 1),
+                                Flexible(
+                                  flex: 10,
+                                  child: AspectRatio(
+                                    aspectRatio: 6/1,
+                                    child: Material(
+                                      elevation: 5,
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: TextFormField(
+                                        autofocus: false,
+                                        decoration: InputDecoration(
+                                          prefixIcon: Icon(
+                                            Icons.meeting_room,
+                                            color: Colors.blue,
+                                          ),
+                                          disabledBorder: InputBorder.none,
+                                          enabledBorder: InputBorder.none,
+                                          focusedBorder: InputBorder.none,
+                                          hintText: 'Enter Room ID:',
+                                        ),
+                                        //
+                                        controller: _controllerID,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )
+                        ),
+                      ],
                     ),
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                        alignment: Alignment.center,
-                        child: MaterialButton(
-                          color: Colors.red,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          elevation: 10,
-                          onPressed: (){
-                            if ( _SerIsReady == true) {
-                              Navigator.pushNamed(
-                                  context,
-                                  '/drawroom',
-                                  arguments: {"roomid": "18"}
-                              );
-                            }
-                          },
-                          child: Text("Join"),
-                        )
-                    )
-                  ),
+                  Expanded(flex: 5,child: Container()),
                 ],
               ),
             ),
